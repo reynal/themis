df1ca9d811dacc9767722b1f2a74bec3457466bd
S. Reynal <reynal@ensea.fr>
2019-01-28 01:27:54 +0100

    Added listeners

---
+++ RPi/src/model/event/SynthParameterEditEvent.java
@@ -0,0 +1,34 @@
+package model.event;
+
+import java.util.EventObject;
+
+import javafx.event.EventHandler;
+
+/**
+ * 
+ * @author sydxrey
+ *
+ * @param <T> the parametrized type of the source parameter, e.g., Double or Enum or Boolean.
+ */
+public class SynthParameterEditEvent<T> extends EventObject {
+
+	private static final long serialVersionUID = 1L;
+	
+	T value;
+
+	/**
+	 * 
+	 * @param source the SynthParameter that changed
+	 * @param state
+	 */
+	public SynthParameterEditEvent(Object source, T value) {
+		super(source);
+		this.value = value;
+	}
+	
+	public T getValue() {
+		return value;
+	}
+	
+	
+}
