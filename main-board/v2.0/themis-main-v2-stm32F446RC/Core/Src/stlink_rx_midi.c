3cbc2ed2599fcf58a583e3284b8006417c627be6
Syd REYNAL <reynal@ensea.fr>
2022-09-02 21:04:29 +0200

    not working yet

--- main-board/v2.0/themis-main-v2-stm32F446RC/Core/Src/stlink_rx_midi.c
+++ main-board/v2.0/themis-main-v2-stm32F446RC/Core/Src/stlink_rx_midi.c
@@ -49,29 +49,6 @@ void stlink_Rx_IRQ_Handler(){
 }
 
 
-// UART RX procedure in HAL:  cf. HAL_UART_IRQHandler() in stm32l4xx_hal_uart.c
-/*
-uint32_t isrflags   = READ_REG(huart->Instance->ISR);
-uint32_t cr1its     = READ_REG(huart->Instance->CR1);
-uint32_t cr3its     = READ_REG(huart->Instance->CR3);
-
-uint32_t errorflags;
-uint32_t errorcode;
-
-errorflags = (isrflags & (uint32_t)(USART_ISR_PE | USART_ISR_FE | USART_ISR_ORE | USART_ISR_NE | USART_ISR_RTOF));
-
-if (((isrflags & USART_ISR_RXNE) != 0U) && ((cr1its & USART_CR1_RXNEIE) != 0U)){
-	uint16_t  data = (uint16_t) READ_REG(huart_STlink->Instance->RDR);
-}
-
-// to stop the UART RX process:
-// Disable the UART Parity Error Interrupt and RXNE interrupts
-CLEAR_BIT(huart->Instance->CR1, (USART_CR1_RXNEIE | USART_CR1_PEIE));
-// Disable the UART Error Interrupt: (Frame error, noise error, overrun error)
-CLEAR_BIT(huart->Instance->CR3, USART_CR3_EIE);
-*/
-
-
 
 
 
